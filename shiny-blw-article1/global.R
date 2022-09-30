4573998469acf9651b0f693f7a22cd648470aa6c
glouarn <gaetan.louarn@inrae.fr>
2022-09-30 16:52:32 +0200

    update path global

--- shiny-blw-article1/global.R
+++ shiny-blw-article1/global.R
@@ -5,13 +5,13 @@ library("corrplot")
 
 
 
-path_ <- here("shiny-blw-article1")
+#path_ <- here("shiny-blw-article1")
 
 #dtoto
-dat2 <- read.table(paste0(path_ ,"/", "tabletoto_corr.csv"), header=T, sep=";", dec=".")
+dat2 <- read.table("tabletoto_corr.csv", header=T, sep=";", dec=".")
 
 #moyenne
-tabres <- read.table(paste0(path_,"/","tabre_moy.csv"), header=T, sep=";", dec=".")
+tabres <- read.table("tabre_moy.csv", header=T, sep=";", dec=".")
 
 ls_traits <- unique(dat2$nomTrait)
 ls_mng <- unique(dat2$MngN)
